0cdda3a9b2048301ec3396a1a1aed3d19c2a64a6
sammanak <naknakss@gmail.com>
2018-12-15 13:36:18 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'tachyons';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
+unregister();
